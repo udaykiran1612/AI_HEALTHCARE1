77213b4912cdf8228acbed22edc3fe6bcd29733d
udaykiran1612 <98377158+udaykiran1612@users.noreply.github.com>
2024-02-29 08:57:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from website import create_app
-import os 
-os.ENVIRON["PF_ENABLE_ONEDNN_OPPS"] = "0"
+import os
+
+# Set TF_ENABLE_ONEDNN_OPTS environment variable to 0
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
+# Now you can proceed with your TensorFlow code
 app = create_app()
 
 
